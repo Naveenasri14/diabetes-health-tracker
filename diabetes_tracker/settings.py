77505230f544e4d99a5f1162e21e6bec7ffbd03c
naveen1072003 <naveenrangaraju450@gmail.com>
2026-03-05 20:53:11 +0530

    updated settings

--- diabetes_tracker/settings.py
+++ diabetes_tracker/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'diabetes_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'diabetes_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres123',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
